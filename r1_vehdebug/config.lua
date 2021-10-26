1cb4d0062414ff1f7bf66c3277aed89fdc491bb5
Re1ease <86981407+Re1ease@users.noreply.github.com>
2021-10-26 16:50:27 +0200

    Added Teleport & Delete
    Ability to configure teleport location and delete vehicle.

--- r1_vehdebug/config.lua
+++ r1_vehdebug/config.lua
@@ -3,11 +3,25 @@ Config = {}
 -- FiveM controls
 -- https://docs.fivem.net/docs/game-references/controls/
 
-Config.Speed = "MPH" -- MPH or KMH
-Config.Notif = "CONSOLE" -- CONSOLE or MYTHIC
+Config.Speed = "MPH" --MPH or KMH
+Config.Notif = "CONSOLE" --CONSOLE or MYTHIC
 
-Config.FixVehicle = 26 -- Currently "C"
-Config.FixVehicleLabel = "C" -- Only for visuals. Replace with "nil" if you dont want it to show.
+--Fixes/repairs vehicle
+Config.FixVehicle = 26 --Currently "C"
+Config.FixVehicleLabel = "C" --Only for visuals. Replace with "nil" if you dont want it to show
 
-Config.Reset = 20 -- Currently "Z"
-Config.ResetLabel = "Z" -- Only for visuals. Replace with "nil" if you dont want it to show.
+--Reset the numbers
+Config.Reset = 20 --Currently "Z"
+Config.ResetLabel = "Z" --Only for visuals. Replace with "nil" if you dont want it to show
+
+--Configure teleport key.
+Config.UseTeleport = true --Or false to disable
+Config.TeleportToCoords = vector3(-1958.9, 2841.6, 32.4) --Teleport location
+Config.TeleportToCoordsHeading = 60.05 --Which way you are looking after being teleported
+Config.TeleportToCoordsKey = 306 --Currently "N"
+Config.TeleportToCoordsLabel = "N" -- Only for visuals. Replace with "nil" if you dont want it to show
+
+--Delete vehicle
+Config.UseDeleteVehicle = true --Or false to disable
+Config.DeleteVehicleKey = 301--Currently "N"
+Config.DeleteVehicleKeyLabel = "M" -- Only for visuals. Replace with "nil" if you dont want it to show
